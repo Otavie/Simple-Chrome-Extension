ad06b1cf5266f9d35f43bb16aa4b8230b6d3b086
Otavie <otavieokuoyo@gmail.com>
2024-08-06 13:36:45 +0100

    Add codes for popup.js and background.js and general house cleaning of others

---
+++ popup/popup.js
@@ -0,0 +1,41 @@
+const locationID = document.getElementById("locationId")
+const startDateID = document.getElementById("start-date")
+const endDateID = document.getElementById("end-date")
+const startBtnID = document.getElementById("startBTN")
+const stopBtnID = document.getElementById("stopBTN")
+
+startBtnID.onclick = function() {
+    const prefs = {
+        locID: locationID.value,
+        start: startDateID.value,
+        end: endDateID.value
+    }
+    chrome.runtime.sendMessage({
+        event: 'onStart',
+        prefs
+    })
+}
+
+stopBtnID.onclick = function() {
+    chrome.runtime.sendMessage({
+        event: 'onStop'
+    })
+
+    console.log("Stop Date is", startDateID.value)
+}
+
+chrome.storage.local.get(["locID", "start", "end"], (result) => {
+    const { locID: storedLocID, start: storedStartDate, end: storedEndDate } = result
+
+    if (storedLocID) {
+        locationID.value = storedLocID
+    }
+
+    if (storedStartDate) {
+        startDateID.value = storedStartDate
+    }
+
+    if (storedEndDate) {
+        endDateID.value = storedEndDate
+    }
+})
